9dcc4cce95f090572e66d37b02519a986e9aa12a
Moscow Cigar <jason@moscowcigar.ru>
2026-05-26 07:21:36 +0000

    fix: 恢复price_tracker代码并修复详情页API 500错误
    - 从feature/price-tracker恢复models.py/views.py/urls.py/admin.py
    - 恢复migration 0001_initial
    - settings.py添加rest_framework和price_tracker到INSTALLED_APPS
    - 主urls.py添加/api/prices/和/prices/路由
    - 根因: fix/coh-brand-matching分支缺少price_tracker完整代码致表不存在

--- moscow_cigar_backend/urls.py
+++ moscow_cigar_backend/urls.py
@@ -6,6 +6,7 @@ from django.conf.urls.static import static
 from cigars import views
 from cigars.auth_views import api_login, api_logout, login_page
 from cigars.views import inventory
+from price_tracker.views import price_dashboard
 from privnote.views import view_note
 
 urlpatterns = [
@@ -17,6 +18,9 @@ urlpatterns = [
     path('api/login/', api_login, name='api_login'),
     path('api/logout/', api_logout, name='api_logout'),
     path('login/', login_page, name='login_page'),
+    path('prices/', price_dashboard, name='price_dashboard'),
+    path('prices/<path:path>', price_dashboard, name='price_dashboard_catchall'),
+    path('api/prices/', include('price_tracker.urls')),
     path('privnote/', include('privnote.urls')),
     path('p/<str:token>/', view_note, name='privnote_view'),
 ]
